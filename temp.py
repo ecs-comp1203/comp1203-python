139019dc4dedcb7d09f824fced6de32cf7c70b74
Kirk Martinez <km@ecs.soton.ac.uk>
2018-10-09 16:05:26 +0100

    clarify channel

--- temp.py
+++ temp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python  
 #
 # use thermistor+8k2ohm on adc, when warm light up red LED, else green
-# ain(1) is pin 2 of the MCP3008 ...
+# ain(1) is CH2 on pin 2 of the MCP3008
 # threshold has to be chosen by experiment
 
 import rpi_io
