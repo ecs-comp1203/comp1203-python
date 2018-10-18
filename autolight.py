7ac5e7ea8787aaee8f5cd286c9ec685942dee808
Josh Curry <josh.curry@talktalk.net>
2018-10-18 13:08:49 +0100

    again

--- autolight.py
+++ autolight.py
@@ -12,8 +12,8 @@ GPIO.setup(LED, GPIO.OUT)
 
 # turn off the light if its dark
 while True:
-    adc = adc.get_ain(0)
-    if adc < 800 :
+    value = adc.get_ain(0)
+    if value < 800 :
         GPIO.output(LED,GPIO.HIGH)
     else :
         GPIO.output(LED,GPIO.LOW)
