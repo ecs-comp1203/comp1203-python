37029f0f00bf8eaac2cc7189c1343dcfacef4a98
Josh Curry <josh.curry@talktalk.net>
2018-10-18 13:08:06 +0100

    another oops

--- autolight.py
+++ autolight.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # turn on LED if it is dark
 # use LDR with adc for input and a gpio to an LED
-import RPi.GPIO
+import RPi.GPIO as GPIO
 import adc
 from time import sleep
 
 LED = 13
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED, GPIO.OUT)
 
 # turn off the light if its dark
 while True:
