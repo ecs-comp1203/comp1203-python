0e485b357158f432669ad4080b5c4889f5d14e4e
Kirk Martinez <km@ecs.soton.ac.uk>
2018-10-23 10:33:14 +0100

    corrected comment

--- autolight.py
+++ autolight.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # turn on LED if it is dark
 # use LDR with adc for input and a gpio to an LED
+# the threshold level of 800 will need adjusting
+# the principle is that the LDR increases in resistance in the dark, the circuit drops in voltage, ADC value drops
 import RPi.GPIO as GPIO
 import adc
 from time import sleep
@@ -10,7 +12,7 @@ LED = 13
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED, GPIO.OUT)
 
-# turn off the light if its dark
+# turn on the light if its dark
 try:
 	while True:
 	    value = adc.get_ain(0)
