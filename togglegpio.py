d34b1fdec50830a0a45ba0aeb436513fc221f4e9
Kirk Martinez <km@ecs.soton.ac.uk>
2018-11-15 10:19:38 +0000

    new fast gpio toggler

---
+++ togglegpio.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# rapidly toggles a GPIO on/off
+# pass it the pin number you want to toggle (13 19 26)
+from sys import argv
+import RPi.GPIO as GPIO
+import os
+
+LED = 13
+
+GPIO.setmode(GPIO.BCM)
+if len(argv) == 2:
+	LED = int(argv[1])
+	GPIO.setup(LED, GPIO.OUT)
+else:
+	print "Arg: 13 19 or 26 for GPIO channel to toggle"
+	exit()
+try:
+    while True:
+        GPIO.output(LED,GPIO.HIGH)
+        GPIO.output(LED,GPIO.LOW)
+
+except KeyboardInterrupt:
+        GPIO.output(LED,GPIO.LOW)
+
